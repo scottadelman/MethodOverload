d0518dfd2da8b1e75b23d7ac73cb7a560e0af37c
Scott <ScottAdelman97@gmail.com>
2022-07-11 12:17:39 -0600

    dollarz

---
+++ MethodOverloading/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    class OverLoad
+    {
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static double Add(double x, double y)
+        {
+            return x + y;
+
+        }
+
+        public static string Add(int x, int y, bool isCheck)
+        {
+            var sum = x + y;
+            if (isCheck == true && sum > 1 || sum == 0)
+            {
+                return $"{sum} dollars";
+            }
+            else if (isCheck == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
+    }
+}
+    
+
---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,8 @@
+﻿using MethodOverloading;
+
+//Console.WriteLine(OverLoad.Add(1, 2));
+
+//Console.WriteLine(OverLoad.Add(6.4, 4.6));
+
+Console.WriteLine(OverLoad.Add(0, 1, true));
+
